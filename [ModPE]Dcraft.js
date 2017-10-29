db8d5773b8fc287a076fc1343b3bed2cc53c8944
Ken Mizz <mizzken16@gmail.com>
2017-10-28 20:39:18 -0400

    Dcraft
    update Chinese(Simplified),English(us) intro

--- [ModPE]Dcraft.js
+++ [ModPE]Dcraft.js
@@ -1,3 +1,23 @@
 /*
 Dcraft
+游乐12制作
+我的个人站:https://kenmizz.github.io
+使用CC-BY-NC-SA4.0版权协议
+协议说明:
+1.署名 — 您必须给出合适的姓名或名称，提供许可协议链接，同时表明如果有修改。您可以合理的方式这样做，但是部分以任何方式暗示许可人同意您或您的使用。
+2.非商业性使用 — 您不得将本作品用于商业目的。 
+3.相同方式共享 — 如果您再混合、转换或者基于本作品进行创作，您必须基于与原先许可协议相同的许可协议 分发您贡献的作品。
+
+没有附加限制 — 您不得适用法律术语或者 技术措施 从而限制其他人做许可协议允许的事情。
+-----
+Dcraft
+Made by KenMizz
+My Site:https://kenmizz.github.io
+Using CC-BY-NC-SA4.0 Copyright License
+License details:
+1.Attribution — You must give appropriate credit, provide a link to the license, and indicate if changes were made. You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use. 
+2.NonCommercial — You may not use the material for commercial purposes. 
+3.ShareAlike — If you remix, transform, or build upon the material, you must distribute your contributions under the same license as the original. 
+
+No additional restrictions — You may not apply legal terms or technological measures that legally restrict others from doing anything the license permits. 
 */
